6263a0d4adfb7be9a7354568ed6136d5203ea5fc
Teddywickstrom <teddy_wickstrom@hotmail.com>
2017-04-27 11:04:05 +0200

    Generated service Cities

--- Feathers-generator/src/services/index.js
+++ Feathers-generator/src/services/index.js
@@ -1,7 +1,9 @@
 const users = require('./users/users.service.js');
 const routes = require('./routes/routes.service.js');
+const cities = require('./cities/cities.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
   app.configure(routes);
+  app.configure(cities);
 };
