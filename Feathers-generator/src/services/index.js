ce82371edcdc6c2ff83e3e5be83939b5241de105
Teddywickstrom <teddy_wickstrom@hotmail.com>
2017-05-09 14:18:54 +0200

    Bort med palace och cities Hejdå! tack för denna tid :)

--- Feathers-generator/src/services/index.js
+++ Feathers-generator/src/services/index.js
@@ -1,13 +1,10 @@
 const users = require('./users/users.service.js');
 const routes = require('./routes/routes.service.js');
-const cities = require('./cities/cities.service.js');
-const palace = require('./palace/palace.service.js');
 const checkpoints = require('./checkpoints/checkpoints.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
   app.configure(routes);
-  app.configure(cities);
-  app.configure(palace);
+
   app.configure(checkpoints);
 };
